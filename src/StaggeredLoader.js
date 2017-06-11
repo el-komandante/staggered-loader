dd9a46530eb708db620b08bc606fef3dd8fd594a
el-komandante <rudydeberry@hotmail.com>
2017-06-11 13:20:51 -0500

    add theming

--- src/StaggeredLoader.js
+++ src/StaggeredLoader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import styled, { keyframes } from 'styled-components'
+import { opacify } from 'polished'
 import _ from 'lodash'
 
 const Container = styled.div`
@@ -29,7 +29,7 @@ const SlideLeft = keyframes`
         transform: translateX(0px);
     }
     25% {
-        transform: translateX(-7px);
+        transform: translateX(2px);
     }
     100% {
         transform: translateX(0px);
@@ -40,42 +40,56 @@ const SlideRight = keyframes`
         transform: translateX(0px);
     }
     25% {
-        transform: translateX(7px);
+        transform: translateX(-2px);
     }
     100% {
         transform: translateX(0px);
     }
 `
+const Rotate360 = keyframes`
+    0% {
+        transform: rotateZ(0deg)
+    }
+    50% {
+        transform: rotateZ(360deg);
+    }
+    100% {
+        transform: rotateZ(0deg);
+    }
+`
 const Bar = styled.div`
     opacity: 0;
-    width: 70px;
+    width: ${ props => (1 / props.numBars) * 100 }%;
     height: 5px;
-    background: red;
+    background: ${ props => props.color ? props.color : 'red' };
     margin: 0px 4px;
-    box-shadow: 0px 0px 40px 2px red;
-    animation: ${ Pulse } 1s ease, ${ props => props.left ? SlideLeft : SlideRight } 1s ease;
-    animation-delay: ${ props => {console.log(props.index);return props.index * 100} }ms;
-    animation-iteration-count: infinite;
+    box-shadow: 0px 0px 40px 2px ${ props => props.color ? opacify(-.3, props.color) : opacify(-.3, 'red') };
+    animation: ${ Pulse } 1s ease infinite;
+        ${'' /* ${ props => props.left ? SlideLeft : SlideRight } 1s ease infinite; */}
+    animation-delay: ${ props => props.index * 100 }ms;
 `
 const BarContainer = styled.div`
     display: flex;
     flex-direction: row;
+    width: 100%;
+    ${'' /* animation: ${ Rotate360 } 2s ease infinite; */}
+    ${'' /* animation-delay: ${ props => (props.numBars / 2) * 100 } */}
 `
 export default class App extends Component {
   renderBars = () => {
-      const { numBars } = this.props
+      const { numBars, color } = this.props
       const leftBars = _.range(numBars / 2).map(num => {
           return (
-              <Bar left={ true } index={ num } key={ `left${num}`}/>
+              <Bar color={ color } numBars={ numBars } left={ true } index={ num } key={ `left${num}`}/>
           )
       })
       const rightBars = _.range(numBars / 2).map(num => {
           return (
-              <Bar index={ (numBars / 2) - 1 - num } key={ `right${num}` }/>
+              <Bar color={ color } numBars={ numBars } index={ (numBars / 2) - 1 - num } key={ `right${num}` }/>
           )
       })
       return (
-          <BarContainer>
+          <BarContainer numBars={ numBars }>
               { leftBars }
               { rightBars }
           </BarContainer>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StaggeredLoader from './App';
+import StaggeredLoader from './StaggeredLoader';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<StaggeredLoader numBars={ 10 } />, document.getElementById('root'));
+ReactDOM.render(<StaggeredLoader color={ 'red' } numBars={ 16 } />, document.getElementById('root'));
 registerServiceWorker();
